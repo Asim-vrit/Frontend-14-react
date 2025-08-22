54d09abff3dc3cc9c19f6e62047824d43acc198d
AsimCh-dev <asim@vrittechnologies.com>
2025-08-22 17:58:51 +0545

    router added

--- src/AppWithRouter.jsx
+++ src/AppWithRouter.jsx
@@ -1,32 +1,87 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Link } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Sidebar from "./components/Sidebar";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: (
+      <>
+        <RouterLayout>
+          <Home />
+        </RouterLayout>
+      </>
+    ),
   },
   {
     path: "/about",
-    element: <About />,
+    element: (
+      <>
+        <RouterLayout>
+          <About />
+        </RouterLayout>
+      </>
+    ),
   },
   {
     path: "/contact",
-    element: <Contact />,
+    element: (
+      <>
+        <RouterLayout>
+          <Contact />
+        </RouterLayout>
+      </>
+    ),
+  },
+  {
+    path: "/privacy-policy",
+    element: (
+      <>
+        <RouterLayout>Privary policy</RouterLayout>
+      </>
+    ),
   },
 ]);
 
 function AppWithRouter() {
   return (
     <>
-      <a href="/">Home</a>
-      <a href="/about">About</a>
-      <a href="/contact">Contact</a>
-
-      <RouterProvider router={router} />
+      <div className="test-router">
+        <RouterProvider router={router} />
+      </div>
+      {/* this is called mounting the router */}
     </>
+  );
+}
+
+function RouterHeader() {
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+    </div>
+  );
+}
+
+function RouterFooter() {
+  return <footer className="mt-auto">Copyright @C</footer>;
+}
+
+function RouterLayout(props) {
+  return (
+    <div className="flex flex-col h-screen">
+      <RouterHeader />
+      <main className="flex gap-2">
+        <div className="p-10 bg-gray-300">
+          <Sidebar />{" "}
+        </div>
+        <div className="w-full block">{props.children}</div>
+      </main>
+      <RouterFooter />
+    </div>
   );
 }
 
