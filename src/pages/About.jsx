5e639e50635f6a5384e4ab194fa1ed6884a3b417
AsimCh-dev <asim@vrittechnologies.com>
2025-08-27 16:16:35 +0545

    query params added in about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import { useSearchParams } from "react-router";
+const data = [
+  { song: "hello", artist: "adele" },
+  { song: "shape of you", artist: "ed sheeran" },
+  { song: "blinding lights", artist: "the weeknd" },
+  { song: "watermelon sugar", artist: "harry styles" },
+  { song: "levitating", artist: "dua lipa" },
+  { song: "good 4 u", artist: "olivia rodrigo" },
+  { song: "stay", artist: "the kid laroi" },
+  { song: "peaches", artist: "justin bieber" },
+  { song: "drivers license", artist: "olivia rodrigo" },
+  { song: "bad habits", artist: "ed sheeran" },
+];
+
+async function fetchProducts(limit) {
+  const res = await fetch("https://fakestoreapi.com/products?limit=" + limit);
+  const data = await res.json();
+  console.log(data);
+}
 
 function About() {
-  return <div style={{ backgroundColor: "rebeccapurple" }}>About</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const q = searchParams.get("q");
+  const limit = searchParams.get("limit");
+  const a = parseInt(searchParams.get("a"));
+  const b = parseInt(searchParams.get("b"));
+  const isNotNumber = isNaN(a) || isNaN(b);
+  fetchProducts(limit);
+
+  return (
+    <>
+      {q && (
+        <>
+          {data
+            .filter((item) => item.song === q)
+            .map((item) => (
+              <div className="bg-amber-600 text-white p-5 text-3xl rounded-xl">
+                {item.artist}
+              </div>
+            ))}
+        </>
+      )}
+      <button
+        onClick={() => {
+          setSearchParams("a=12&b=24");
+        }}
+      >
+        Change Params
+      </button>
+      <div className="bg-purple-700 text-white">
+        sum of searchParams= {isNotNumber ? "Your input is incorrect" : a + b}
+      </div>
+    </>
+  );
 }
 
 export default About;
