f6b7d7008afb67a3562c450039232c29c19798e3
AsimCh-dev <asim@vrittechnologies.com>
2025-09-19 17:19:08 +0545

    login fixed

--- src/AppWithRouter.jsx
+++ src/AppWithRouter.jsx
@@ -5,7 +5,7 @@ import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
 import Users from "./pages/Users";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import { useUserContext } from "./providers/useUserContext";
 import HookForm from "./pages/HookForm";
 
